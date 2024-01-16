a8f96f1a04cc3c17d1de76c02fed3e23a39a9160
rahulrao1607 <aheerahul1002@gmail.com>
2024-01-16 11:23:22 +0530

    changes in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     // get rid of cors error
     proxy:{
       "/api":{
-        target: "http://localhost:5000",
+        target: "https://threads-clone-nu-nine.vercel.app/",
         changeOrigin: true,
         secure: false,
       }
